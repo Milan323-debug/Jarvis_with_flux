c93cddaf23bc49b3d7b70b28b3c8c0fa32f2f776
Milan <milan212134@gmail.com>
2025-03-05 09:44:53 +0530

    Committing changes before Tailwind CSS upgrade

--- Downloads/Chat-App/frontend/vite.config.js
+++ Downloads/Chat-App/frontend/vite.config.js
@@ -1,10 +1,9 @@
+// filepath: /c:/Users/Dell/Downloads/Chat-App/frontend/vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),
-    tailwindcss(),
-  ],
+  plugins: [react(), tailwindcss()],
 })
